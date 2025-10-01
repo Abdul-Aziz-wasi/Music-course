20ffdcbc236d90eb7940666d56e242168940a46d
Abdul Aziz <abdulaziz758015@gmail.com>
2025-10-01 17:24:43 +0600

    added featuredSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { FeaturedSection } from '@/components/FeaturedSection';
 import HeroSection from '@/components/HeroSection';
 import React from 'react';
 
@@ -6,6 +7,7 @@ const Home = () => {
     <div>
      <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.04]">
         <HeroSection />
+        <FeaturedSection></FeaturedSection>
       </main>
     </div>
   );
