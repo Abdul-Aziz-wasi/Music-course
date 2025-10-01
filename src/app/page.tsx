04962b540121809e3c0c2881f9b4dd576348996b
Abdul Aziz <abdulaziz758015@gmail.com>
2025-10-01 17:48:39 +0600

    added why choose us section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { FeaturedSection } from '@/components/FeaturedSection';
 import HeroSection from '@/components/HeroSection';
+import WhychooseUs from '@/components/WhychooseUs';
 import React from 'react';
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
      <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.04]">
         <HeroSection />
         <FeaturedSection></FeaturedSection>
+        <WhychooseUs></WhychooseUs>
       </main>
     </div>
   );
